03afb34cb7a800d45470fd9042ccdd655fb4cba3
Benito Ishimwe <benishimwe31@gmail.com>
2022-05-02 19:18:33 +0200

    Making API Calls - React - Promises

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import API from './API';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <API />
+      </header>
+    </div>
+  );
+}
+
+export default App;
